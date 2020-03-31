15a0dea7c8af9bd52b2fe9d14f1899e79f201575
harshita269 <harshitakhare2626@gmail.com>
2020-03-31 14:12:49 +0530

    API for Type 8 created

--- app/upload/api/serializers.py
+++ app/upload/api/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
-from ..models import Type1
+from ..models import Type1, Type8
 
 
 class Type1Serializers(serializers.ModelSerializer):
+    """Serializer for Type1 data"""
+
     class Meta:
         model = Type1
+        fields = "__all__"
+
+
+class Type8Serializers(serializers.ModelSerializer):
+    """Serializer for Type8 data"""
+
+    class Meta:
+        model = Type8
         fields = "__all__"
